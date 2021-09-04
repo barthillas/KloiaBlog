96f20204ddb967ae8539e3def2f8adfdbb064dfa
Ümit Veysel Okçu <umitveysel.okcu@sodexo.com>
2021-09-05 02:19:07 +0300

    exeptionMiddleware

---
+++ Infrastructure/ApiBase/Extension/HttpContextExtensions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Net.Mime;
+using System.Text;
+using System.Threading.Tasks;
+using ApiBase.Middlewares;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace ApiBase.Extension
+{
+    internal static class HttpContextExtensions
+    {
+        internal static Task HandleExceptionAsync(this HttpContext context,
+            Exception exception)
+        {
+            context.Response.ContentType = MediaTypeNames.Application.Json;
+
+            var exceptionMessage = new StringBuilder();
+
+            exceptionMessage.AppendLine(exception.Message);
+            exceptionMessage.AppendLine(exception.StackTrace);
+            while (exception.InnerException != null)
+            {
+                var innerException = exception.InnerException;
+                exceptionMessage.AppendLine(innerException.Message);
+                exceptionMessage.AppendLine(innerException.StackTrace);
+                exception = exception.InnerException;
+
+            }
+
+            var responseText = JsonConvert.SerializeObject(exceptionMessage.ToString(), new JsonSerializerSettings
+            {
+                ContractResolver = new DefaultContractResolver()
+                {
+                    NamingStrategy = new CamelCaseNamingStrategy(),
+                },
+                Formatting = Formatting.Indented,
+            });
+
+            return context.Response.WriteAsync(responseText);
+        }
+    }
+}
---
+++ Infrastructure/ApiBase/Middlewares/ExceptionHandling.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using ApiBase.Extension;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace ApiBase.Middlewares
+{
+    public class ExceptionHandlingMiddleware
+    {
+        private readonly ILogger _logger;
+        private readonly RequestDelegate _next;
+  
+
+        public ExceptionHandlingMiddleware(RequestDelegate next, ILoggerFactory loggerFactory)
+        {
+            this._next = next;
+            this._logger = loggerFactory.CreateLogger<ExceptionHandlingMiddleware>();
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            if (context.Request.Body.CanSeek)
+            {
+                context.Request.Body.Position = 0;
+                using var reader = new StreamReader(context.Request.Body);
+                await reader.ReadToEndAsync().ConfigureAwait(false);
+            }
+
+            try
+            {
+                await this._next(context).ConfigureAwait(false);
+            }
+            catch (Exception exception)
+            {
+                    await context.HandleExceptionAsync(exception ).ConfigureAwait(false);
+                    this._logger.LogError(exception, context.Request.Path); 
+            }
+        }
+    }
+}
