2b8e7216e28f922454b1077dc747405f59da2085
Ümit Veysel Okçu <umitveysel.okcu@sodexo.com>
2021-09-05 02:25:52 +0300

    GetAll and GetById endpoints defined

---
+++ Services/Article/Src/ArticleService.Api/Controllers/ArticleController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Abstraction.Dto;
+using ApiBase.Controllers;
+using ArticleService.Domain.Entities;
+using ArticleService.Infrastructure.Context;
+using Data.UnitOfWork;
+using Microsoft.AspNet.OData;
+using Microsoft.AspNet.OData.Routing;
+using static Microsoft.AspNetCore.Http.StatusCodes;
+
+namespace ArticleService.Api.Controllers
+{
+    [Route("[controller]")]
+    public class ArticleController : ODataControllerBase
+    {
+        private readonly IUnitOfWork<ArticleDbContext> _unitOfWork;
+
+        private readonly ILogger<ArticleController> _logger;
+
+        public ArticleController(ILogger<ArticleController> logger, ArticleDbContext context, IUnitOfWork<ArticleDbContext> unitOfWork) 
+        {
+            _logger = logger;
+            _unitOfWork = unitOfWork;
+        }
+
+        
+        [EnableQuery]
+        [ODataRoute("Articles({id})")]
+        public async Task<IActionResult> Get([FromODataUri] int id)
+        {
+            var data = _unitOfWork.GetRepository<Article>()
+                .Find(x => x.ArticleId == id)
+                    .Select(x => new ArticleDto
+                    {ArticleId = x.ArticleId, Author = x.Author, ArticleContent = x.ArticleContent});
+            
+            return Ok(data);
+        }
+
+       [ODataRoute("Articles")]
+       [EnableQuery]
+       public async Task<IActionResult> Get()
+       {
+
+           var asd = await _unitOfWork.GetRepository<Article>().GetAllAsync(new CancellationToken())
+               .ConfigureAwait(false);
+           
+           
+           return Ok( _unitOfWork.GetRepository<Article>().GetAll());
+       }
+    }
+}
---
+++ Services/Review/Src/ReviewService.Api/Controllers/ReviewController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Abstraction.Dto;
+using ApiBase.Controllers;
+using Data.UnitOfWork;
+using Microsoft.AspNet.OData;
+using Microsoft.AspNet.OData.Routing;
+using ReviewService.Domain.Entities;
+using ReviewService.Infrastructure.Context;
+
+namespace ReviewService.Api.Controllers
+{
+    [Route("[controller]")]
+    public class ReviewController : ODataControllerBase
+    {
+        private readonly IUnitOfWork<ReviewDbContext> _unitOfWork;
+
+        private readonly ILogger<ReviewController> _logger;
+
+        public ReviewController(ILogger<ReviewController> logger, IUnitOfWork<ReviewDbContext> unitOfWork)
+        {
+            _logger = logger;
+            _unitOfWork = unitOfWork;
+        }
+
+        [EnableQuery]
+        [ODataRoute("Reviews({id})")]
+        public async Task<IActionResult> Get([FromODataUri] int id)
+        {
+            var data = _unitOfWork.GetRepository<Review>()
+                .Find(x => x.ReviewId == id)
+                .Select(x => new ReviewDto()
+                    {ArticleId = x.ArticleId, Reviewer = x.Reviewer, ReviewId = x.ReviewId, ReviewContent = x.ReviewContent});
+            
+            return Ok(data);
+        }
+
+        [ODataRoute("Reviews")]
+        [EnableQuery]
+        public async Task<IActionResult> Get()
+        {
+
+            var asd = await _unitOfWork.GetRepository<Review>().GetAllAsync(new CancellationToken())
+                .ConfigureAwait(false);
+           
+           
+            return Ok( _unitOfWork.GetRepository<Review>().GetAll());
+        }
+    }
+}
