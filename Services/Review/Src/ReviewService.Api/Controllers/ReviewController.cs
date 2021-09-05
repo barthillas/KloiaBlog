1e4383d0167856ff785b2b23702f238118442fc8
Ümit Veysel Okçu <umitveysel.okcu@sodexo.com>
2021-09-05 19:15:55 +0300

    add article into review data

--- Services/Review/Src/ReviewService.Api/Controllers/ReviewController.cs
+++ Services/Review/Src/ReviewService.Api/Controllers/ReviewController.cs
@@ -1,4 +1,6 @@
 ﻿
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -11,7 +13,9 @@ using Data.UnitOfWork;
 using MediatR;
 using Microsoft.AspNet.OData;
 using Microsoft.AspNet.OData.Routing;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using ReviewService.Abstraction.Command;
 using ReviewService.Domain.Entities;
 using ReviewService.Infrastructure.Context;
@@ -26,37 +30,44 @@ namespace ReviewService.Api.Controllers
     {
         private readonly IUnitOfWork<ReviewDbContext> _unitOfWork;
         private readonly ICommandSender _mediator;
+        private readonly IHttpContextAccessor _httpContextAccessor;
         private readonly ODataClient _oDataClient;
 
-        public ReviewController(IUnitOfWork<ReviewDbContext> unitOfWork, ODataClient oDataClient, ICommandSender mediator)
+        public ReviewController(IUnitOfWork<ReviewDbContext> unitOfWork, ODataClient oDataClient, ICommandSender mediator, IHttpContextAccessor httpContextAccessor)
         {
             _unitOfWork = unitOfWork;
             _oDataClient = oDataClient;
             _mediator = mediator;
+            _httpContextAccessor = httpContextAccessor;
         }
 
         [EnableQuery]
         [ODataRoute("Reviews({id})")]
         public async Task<IActionResult> Get([FromODataUri] int id)
         {
-            var data = _unitOfWork.GetRepository<Review>()
-                .Find(x => x.ReviewId == id)
-                .Select(x => new ReviewDto()
-                    {ArticleId = x.ArticleId, Reviewer = x.Reviewer, ReviewId = x.ReviewId, ReviewContent = x.ReviewContent});
+            var reviewDtos = await _unitOfWork.GetRepository<Review>().CreateQuery(x => x.ReviewId == id).Select(x =>
+                new ReviewDto
+                {
+                    ArticleId = x.ArticleId, Reviewer = x.Reviewer, ReviewId = x.ReviewId,
+                    ReviewContent = x.ReviewContent
+                }).ToListAsync(cancellationToken: new CancellationToken()).ConfigureAwait(false);
             
-            return Ok(data);
+            return Ok(MergeReviewWithArticle(reviewDtos, await GetArticle().ConfigureAwait(false)));
         }
 
         [ODataRoute("Reviews")]
         [EnableQuery]
         public async Task<IActionResult> Get()
         {
-
-            var data = await _unitOfWork.GetRepository<Review>().GetAllAsync(new CancellationToken())
+            var isInbound = _httpContextAccessor.HttpContext?.Request.Headers["InboundRequest"].Count;
+            if (isInbound > 0)
+            {
+                return Ok(await _unitOfWork.GetRepository<Review>().CreateQuery().ToListAsync());
+            }
+            var reviews = await _unitOfWork.GetRepository<Review>().CreateQuery().Select(x => new ReviewDto
+                    {ArticleId = x.ArticleId, ReviewId = x.ReviewId, Reviewer = x.Reviewer, ReviewContent = x.ReviewContent}).ToListAsync( new CancellationToken())
                 .ConfigureAwait(false);
-           
-           
-            return Ok( _unitOfWork.GetRepository<Review>().GetAll());
+            return Ok(MergeReviewWithArticle(reviews, await GetArticle().ConfigureAwait(false)));
         }
         
         [HttpPost("Create")]
@@ -79,5 +90,39 @@ namespace ReviewService.Api.Controllers
             var data = await _mediator.SendAsync(command).ConfigureAwait(false);
             return ProduceResponse(data);
         }
+        
+        private async Task<IEnumerable<IDictionary<string, object>>> GetArticle()
+        {
+            var isInbound = _httpContextAccessor.HttpContext?.Request.Headers["InboundRequest"].Count;
+            if (isInbound > 0)
+            {
+                return new List<IDictionary<string, object>>();
+            }
+            return await _oDataClient
+                .For("Article")
+                .FindEntriesAsync(new ODataFeedAnnotations()).ConfigureAwait(false);
+        }
+
+        private static IQueryable<ReviewDto> MergeReviewWithArticle(IEnumerable<ReviewDto> data, IEnumerable<IDictionary<string, object>> articles)
+        {
+            var reviewDtos = data.ToList();
+            foreach (var reviewDto in reviewDtos)
+            {
+                var enumerable = articles.Where(x => (int) x["ArticleId"] == reviewDto.ArticleId).ToList();
+                if (!enumerable.Any()) continue;
+                var article = enumerable.Select(dictionary => new ArticleDto()
+                    {
+                        ArticleId = (int) dictionary["ArticleId"],
+                        Author = (string) dictionary["Author"],
+                        ArticleContent = (string) dictionary["ArticleContent"],
+                        Title = (string) dictionary["Title"],
+                    })
+                    .FirstOrDefault();
+
+                reviewDto.Article = article;
+            }
+
+            return reviewDtos.AsQueryable();
+        } 
     }
 }
