1eccf1a057532d36d1a03340c381da6b7e08f8db
Ümit Veysel Okçu <umitveysel.okcu@sodexo.com>
2021-09-05 18:42:40 +0300

    Review C U D Enpoints

--- Services/Review/Src/ReviewService.Api/Controllers/ReviewController.cs
+++ Services/Review/Src/ReviewService.Api/Controllers/ReviewController.cs
@@ -1,31 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
+﻿
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Abstraction.Dto;
 using ApiBase.Controllers;
+using ApiBase.Response;
+using ArticleService.Abstraction.Command;
+using Data.CQRS;
 using Data.UnitOfWork;
+using MediatR;
 using Microsoft.AspNet.OData;
 using Microsoft.AspNet.OData.Routing;
+using Microsoft.AspNetCore.Mvc;
+using ReviewService.Abstraction.Command;
 using ReviewService.Domain.Entities;
 using ReviewService.Infrastructure.Context;
+using Simple.OData.Client;
 
 namespace ReviewService.Api.Controllers
 {
-    [Route("[controller]")]
+    [ApiController]
+    [ApiVersion("1.0")]
+    [Route("api/[controller]")]
     public class ReviewController : ODataControllerBase
     {
         private readonly IUnitOfWork<ReviewDbContext> _unitOfWork;
+        private readonly ICommandSender _mediator;
+        private readonly ODataClient _oDataClient;
 
-        private readonly ILogger<ReviewController> _logger;
-
-        public ReviewController(ILogger<ReviewController> logger, IUnitOfWork<ReviewDbContext> unitOfWork)
+        public ReviewController(IUnitOfWork<ReviewDbContext> unitOfWork, ODataClient oDataClient, ICommandSender mediator)
         {
-            _logger = logger;
             _unitOfWork = unitOfWork;
+            _oDataClient = oDataClient;
+            _mediator = mediator;
         }
 
         [EnableQuery]
@@ -45,11 +52,32 @@ namespace ReviewService.Api.Controllers
         public async Task<IActionResult> Get()
         {
 
-            var asd = await _unitOfWork.GetRepository<Review>().GetAllAsync(new CancellationToken())
+            var data = await _unitOfWork.GetRepository<Review>().GetAllAsync(new CancellationToken())
                 .ConfigureAwait(false);
            
            
             return Ok( _unitOfWork.GetRepository<Review>().GetAll());
+        }
+        
+        [HttpPost("Create")]
+        public async Task<Response<Unit>> Create([FromForm] CreateReviewCommand command)
+        {
+            var data = await _mediator.SendAsync(command).ConfigureAwait(false);
+            return ProduceResponse(data);
+        }
+        
+        [HttpDelete("Delete")]
+        public async Task<Response<Unit>> Delete([FromForm] DeleteReviewCommand command)
+        {
+            var data = await _mediator.SendAsync(command).ConfigureAwait(false);
+            return ProduceResponse(data);
+        }
+        
+        [HttpPut("Update")]
+        public async Task<Response<Unit>> Update([FromForm] UpdateReviewCommand command)
+        {
+            var data = await _mediator.SendAsync(command).ConfigureAwait(false);
+            return ProduceResponse(data);
         }
     }
 }
