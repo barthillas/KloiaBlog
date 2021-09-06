b5657b4d646c29dfdb2e5572ab754926e40592bb
Ümit Veysel Okçu <umitveysel.okcu@sodexo.com>
2021-09-07 00:51:04 +0300

    ODataControllers

---
+++ Services/Article/Src/ArticleService.Api/Controllers/ArticleODataController.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+using ApiBase.Controllers;
+using ArticleService.Domain.Entities;
+using ArticleService.Infrastructure.Context;
+using Data.UnitOfWork;
+using Microsoft.AspNet.OData;
+using Microsoft.AspNet.OData.Routing;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ArticleService.Api.Controllers
+{
+    public class ArticleODataController : ODataControllerBase
+    {
+        private readonly IUnitOfWork<ArticleDbContext> _unitOfWork;
+
+        public ArticleODataController(IUnitOfWork<ArticleDbContext> unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [EnableQuery]
+        [ODataRoute("Articles")]
+        public IActionResult Get()
+        {
+            var albums = _unitOfWork.GetRepository<Article>().CreateQuery();
+            return Ok(albums);
+        }
+
+        [EnableQuery]
+        [ODataRoute("Articles({id})")]
+        public IActionResult Get(int id)
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            var album = _unitOfWork.GetRepository<Article>().CreateQuery().Where(x => x.ArticleId == id);
+            
+            return Ok(album);
+        }
+    }
+}
---
+++ Services/Review/Src/ReviewService.Api/Controllers/ReviewODataController.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+using ApiBase.Controllers;
+using Data.UnitOfWork;
+using Microsoft.AspNet.OData;
+using Microsoft.AspNet.OData.Routing;
+using Microsoft.AspNetCore.Mvc;
+using ReviewService.Domain.Entities;
+using ReviewService.Infrastructure.Context;
+
+namespace ReviewService.Api.Controllers
+{
+    public class ReviewODataController : ODataControllerBase
+    {
+        private readonly IUnitOfWork<ReviewDbContext> _unitOfWork;
+
+        public ReviewODataController(IUnitOfWork<ReviewDbContext> unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [EnableQuery]
+        [ODataRoute("Reviews")]
+        public IActionResult Get()
+        {
+            var albums = _unitOfWork.GetRepository<Review>().CreateQuery();
+            return Ok(albums);
+        }
+
+        [EnableQuery]
+        [ODataRoute("Reviews({id})")]
+        public IActionResult Get(int id)
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            var album = _unitOfWork.GetRepository<Review>().CreateQuery().Where(x => x.ReviewId == id);
+            return Ok(album);
+        }
+    }
+}
